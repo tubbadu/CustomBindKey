5834f5c3e5d2533962f13fafb00b4ee808d1bdde
tubbadu <ale.ribau@gmail.com>
2021-03-20 19:02:55 +0100

    now it works, but I don't use it because I can't assign shortcuts as I like

--- CustomBindKeys.py
+++ CustomBindKeys.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
+# funziona !
+# cmd: python3 "/home/tubbadu/code/GitHub/CustomBindKeys/CustomBindKeys.py" §
+
 import pyperclip
 import sys
 import subprocess
 ch = sys.argv[1]
 
-pastePath = '/home/tubbadu/code/GitHub/CustomBindKeys/bind_paste.py'
+pastePath = '/home/tubbadu/code/GitHub/CustomBindKeys/CustomBindKeys_paste.py'
 
 def spawn_program_and_die(program, exit_code=0): #copiata lol
-    """
-    Start an external program and exit the script 
-    with the specified return code.
-
-    Takes the parameter program, which is a list 
-    that corresponds to the argv of your command.
-    """
-    # Start the external program
-    subprocess.Popen(program)
-    # We have started the program, and can suspend this interpreter
-    sys.exit(exit_code)
+	subprocess.Popen(program)
+	sys.exit(exit_code)
 
 pyperclip.copy(ch)
 spawn_program_and_die(['python3', pastePath, ch])
+
